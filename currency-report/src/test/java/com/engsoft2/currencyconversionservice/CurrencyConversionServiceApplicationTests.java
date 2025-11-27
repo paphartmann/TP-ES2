3c06cb3b15afa70c2526aaeca75b211ea9efae4e
brunobaa <brunobarbosagoku@gmail.com>
2025-11-26 23:22:35 -0300

    teste2

--- currency-report/src/test/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ currency-report/src/test/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -2,12 +2,29 @@ package com.engsoft2.currencyreport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest(properties = {
 	"eureka.client.enabled=false",
-	"spring.cloud.discovery.enabled=false"
+	"spring.cloud.discovery.enabled=false",
+	"spring.cloud.openfeign.enabled=false" // evita criar cliente Feign real nos testes
 })
 class CurrencyReportApplicationTests {
+
+	@TestConfiguration
+	static class StubConfig {
+		@Bean
+		CurrencyExchangeProxy currencyExchangeProxy() {
+			return (from, to) -> {
+				CurrencyExchangeDto dto = new CurrencyExchangeDto();
+				dto.setFrom(from);
+				dto.setTo(to);
+				dto.setConversionMultiple(1.0);
+				return dto;
+			};
+		}
+	}
 
 	@Test
 	void contextLoads() {
