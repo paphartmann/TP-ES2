5de112db0e117e1f6cd752af89dbdf80458e9b99
brunobaa <brunobarbosagoku@gmail.com>
2025-11-26 23:17:50 -0300

    teste

--- currency-history/src/test/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ currency-history/src/test/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -1,13 +1,17 @@
-package com.engsoft2.currencyconversionservice;
+package com.engsoft2.currencyhistory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CurrencyConversionServiceApplicationTests {
+@SpringBootTest(properties = {
+	"eureka.client.enabled=false",
+	"spring.cloud.discovery.enabled=false"
+})
+class CurrencyHistoryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// sanity check: context must load without external infra
 	}
 
 }
